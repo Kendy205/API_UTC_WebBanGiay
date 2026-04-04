b564801936d04b551b6c351d1d5203d727e6045e
Kendy205 <thanhtung002zzz@gmail.com>
2026-04-04 20:04:31 +0700

    delete controllerCheckout

--- WebBanHang/Controllers/CartController/CheckoutController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace WebBanHang.Controllers.CartController
-{
-    [Route("api/[controller]")]
-    public class CheckoutController : ControllerBase
-    {
-    }
-}
